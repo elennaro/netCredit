f7a79a311bb79315011bd7de6c65b291562ed902
Alexander Art <elennaro@gmail.com>
2016-02-13 16:02:25 +0400

    * init

---
+++ src/main/java/art/alex/NetcreditTestApplication.java
@@ -0,0 +1,12 @@
+package art.alex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcreditTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetcreditTestApplication.class, args);
+	}
+}
